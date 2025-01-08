c1bd1f97938f3e4cd78a54a7cd3b43f359cd9787
Md Allama Ikbal <ikbal559@gmail.com>
2025-01-09 02:42:02 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
+ 
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
@@ -29,10 +21,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+ 
 
 require __DIR__.'/auth.php';
